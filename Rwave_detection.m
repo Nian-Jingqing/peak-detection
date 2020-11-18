86934b7acf3a408b83cd8cd5489b855b8a8b97c1
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2020-11-18 20:16:56 +0200

    בסוף זה יגמר

--- Rwave_detection.m
+++ Rwave_detection.m
@@ -1,8 +1,12 @@
 function [filtered_peaks] = Rwave_detection(ECG_signal)
-% This function receives a ECG signal, and returns a vector that contains the indexes in which the R waves appear in the ECG signal
+% This function receives a ECG signal, and returns a vector that contains the indexes in which the R waves appear in the ECG signal.
+% in order to do so, the function uses **************
 tic
+% Set sample frequency
 fs = 1000;
 
+% Compute first and second derivative
+% First allocate derivative vectors to improve runtime
 first_deriv = zeros(1,length(ECG_signal));
 second_deriv = zeros(1,length(ECG_signal));
 
@@ -10,10 +14,10 @@ for n = 3: (length(ECG_signal)-2)
    
     first_deriv(n) = abs(ECG_signal(n+1)-ECG_signal(n-1));
     second_deriv(n) = abs(ECG_signal(n+2)-2*ECG_signal(n)+ECG_signal(n-2));
-    
+ 
 end
 
-% Normalize
+% Normalize the derivatives to a scale of 0-1
 min_val = min(first_deriv);
 max_val = max(first_deriv);
 norm_first_deriv = (first_deriv-min_val)/(max_val-min_val);
@@ -25,32 +29,44 @@ norm_second_deriv = (second_deriv-min_val)/(max_val-min_val);
 linear_combination = 3*norm_first_deriv + 1.1*norm_second_deriv;
 
 
-% Finding the indexes of the R peaks, using a threshold of 0.1.
-[~,peak_ind_comb] = findpeaks(linear_combination,'MinPeakProminence',0.5,'MinPeakDistance',fs*0.4);
-% Coverting indexes vector to time vector
+% Finding the indexes of the R peaks, using a minimum prominence 0f 0.6 and a minimum distance 0f 0.6 seconds
+[~,peak_ind] = findpeaks(linear_combination,'MinPeakProminence',0.6,'MinPeakDistance',fs*0.4);
 
-
-% Checking if 6 out of 8 of next samples reach threshold
-filtered_peaks = zeros(1,length(peak_ind_comb));
+% Checking if 6 out of 8 of surrounding samples reach threshold
+% Allocating index vector to improve runtime
+filtered_peaks = zeros(1,length(peak_ind));
 k=0;
-for i = 1:length(peak_ind_comb)
+for i = 1:length(peak_ind)
     
-    index = peak_ind_comb(i);
-    check_vec = linear_combination(index+1:index+8);
+    index = peak_ind(i);
+    check_vec = linear_combination(index-4:index+4);
     threshold = 0.2;
     
-    if sum(check_vec>threshold) >= 6   
+    if sum(check_vec>threshold) >= 7   
         k = k+1;
-        filtered_peaks(k) = peak_ind_comb(i);      
+        filtered_peaks(k) = peak_ind(i);      
     end 
 end
+
+% Cut unnecessary zeros out of the vector
 filtered_peaks = filtered_peaks(1:k);
 
+% In order to adjust the indexes to the peak exactly, we change it to the index
+% of the maximum point in a window of 350 samples to each direction
+for i = 1:length(filtered_peaks)
+    
+    index = filtered_peaks(i);
+    
+    if (index>350) && (index<(length(ECG_signal)-350))
+        window = 350;
+        check_vec = ECG_signal(index-window:index+window);
+        [~,max_ind] = max(check_vec);
+        ind_change = max_ind-(window+1);
+        filtered_peaks(i)= index+ind_change;
+        
+    end
+end
 
-% to be removed!!!
-figure(1) 
-findpeaks(linear_combination,'MinPeakProminence',0.5,'MinPeakDistance',fs*0.4);
-% to be removed!!!
 toc
 end
 
