3e6f1c837debfba68a1f7a964e18026e363b56fe
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2020-11-21 15:40:44 +0200

    hust a little bit more

--- Rwave_detection2.m
+++ Rwave_detection2.m
@@ -1,4 +1,4 @@
-function [QRS_comp] = Rwave_detection2(ECG_signal)
+function [R_peaks] = Rwave_detection2(ECG_signal)
 % This function receives a ECG signal, and returns a vector that contains the indexes in which the R waves appear in the ECG signal.
 % in order to do so, the function uses **************
 tic
@@ -15,21 +15,27 @@ for n = 3: (length(ECG_signal)-2)
  
 end
 
+% Normalize the derivative to a scale of 0-1
+min_val = min(first_deriv);
+max_val = max(first_deriv);
+norm_first_deriv = (first_deriv-min_val)/(max_val-min_val);
+
 % Find maximum slope to determine slope threshold
 
 
-max_points = findpeaks(first_deriv,'NPeaks',round(length(ECG_signal)*0.015),'SortStr','descend','MinPeakDistance',fs);
-max_slope = max_points(round(end/2));
-slope_threshold = 0.01*max_slope;
+%max_points = findpeaks(first_deriv,'NPeaks',round(length(ECG_signal)*0.015),'SortStr','descend','MinPeakDistance',fs);
+%max_slope = max_points(round(end/2));
+mean_slope = mean(norm_first_deriv);
+slope_threshold = 1.1*mean_slope;
 
 i=1;
-Min_Distance = fs*0.3;
+Min_Distance = fs*0.2;
 QRS_comp = zeros(1,length(ECG_signal));
 k=0;
-while i<(length(first_deriv)-Min_Distance)
+while i<(length(norm_first_deriv))
     
-    if first_deriv(i) > slope_threshold
-        % If the slope reaches treshold, save index and jump 0.2 second
+    if norm_first_deriv(i) > slope_threshold
+        % If the slope reaches treshold, save index and jump 0.2 second forward
         k = k+1;
         QRS_comp(k) = i;
         i = i + Min_Distance;
@@ -43,7 +49,7 @@ QRS_comp = QRS_comp(1:k);
 
 % In order to adjust the indexes to the peak exactly, we change it to the index
 % of the maximum point in a window of 350 samples to each direction
-window = 0.4*fs;
+window = 0.25*fs;
 
 for i = 1:length(QRS_comp)
     
@@ -61,6 +67,17 @@ end
 
 % Delete replicated peaks if exsists
 QRS_comp = unique(QRS_comp);
+
+
+% Normalize the original signal to a scale of 0-1
+min_val = min(ECG_signal);
+max_val = max(ECG_signal);
+ECG_signal_norm = (ECG_signal-min_val)/(max_val-min_val);
+
+% Finding the peaks of the original signal
+ [~,ECG_peaks] = findpeaks(ECG_signal_norm,'MinPeakProminence',0.3,'MinPeakDistance',fs*0.3);
+
+R_peaks = intersect(QRS_comp,ECG_peaks);
 
 toc
 end
--- main_R_detection.m
+++ main_R_detection.m
@@ -17,38 +17,74 @@ clear sig1 sig2 sig3 sig4
 
 %% R-wave detection 
 %call function to detect R-waves 
-R011 = Rwave_detection(ECG_305338691_01); %R011 = index of R waves in signal
-R012 = Rwave_detection(ECG_305338691_02); %R012 = index of R waves in signal
-R021 = Rwave_detection(ECG_308317361_01); %R021 = index of R waves in signal
-R022 = Rwave_detection(ECG_308317361_02); %R022 = index of R waves in signal
+R011 = Rwave_detection2(ECG_305338691_01); %R011 = index of R waves in signal
+R012 = Rwave_detection2(ECG_308317361_01); %R012 = index of R waves in signal
+R021 = Rwave_detection2(ECG_305338691_02); %R021 = index of R waves in signal
+R022 = Rwave_detection2(ECG_308317361_02); %R022 = index of R waves in signal
 
 %% Plots
 %Add required plots
 % Define time vector 
 fs = 1000; 
 T = 1/fs; 
-%t = (0:length(ECG_signal)-1)*T;
+t_01 = (0:length(ECG_305338691_01)-1)*T;
+t_02 = (0:length(ECG_305338691_02)-1)*T;
 
 
 figure(1)
-plot(ECG_305338691_01)
-hold on
-plot(R011,ECG_305338691_01(R011),'o')
+plot(t_01(12*fs:17*fs),ECG_305338691_01(12*fs:17*fs))
+title('first ECG signal in seconds 12-17')
+xlabel('Time (sec)')
+ylabel('Voltage (micro-Volt)')
+axis tight
 
 figure(2)
-plot(ECG_305338691_02)
-hold on
-plot(R012,ECG_305338691_02(R012),'o')
+plot(t_02(12*fs:17*fs),ECG_305338691_02(12*fs:17*fs))
+title('second ECG signal in seconds 12-17')
+xlabel('Time (sec)')
+ylabel('Voltage (micro-Volt)')
+axis([12 17 min(ECG_305338691_02) max(ECG_305338691_02)])
 
 figure(3)
-plot(ECG_308317361_01)
+plot(t_01,ECG_305338691_01)
+title('first ECG signal in seconds 20-25')
+xlabel('Time (sec)')
+ylabel('Voltage (micro-Volt)')
 hold on
-plot(R021,ECG_308317361_01(R021),'o')
+plot(T*R011,ECG_305338691_01(R011),'o')
+hold off
+axis([20 25 min(ECG_305338691_01) max(ECG_305338691_01)])
 
 figure(4)
-plot(ECG_308317361_02)
+plot(t_02,ECG_305338691_02)
+title('second ECG signal in seconds 20-25')
+xlabel('Time (sec)')
+ylabel('Voltage (micro-Volt)')
 hold on
-plot(R022,ECG_308317361_02(R022),'o')
+plot(T*R021,ECG_305338691_02(R021),'o')
+hold off
+axis([20 25 min(ECG_305338691_02) max(ECG_305338691_02)])
+
+% Computing heart rate for R011 and R021
+% Calculating the Heart rate for each heartbeat, by subtracting the time between each consecutive R wave.
+HR1 = zeros(1,length(t_01));
+for i = 1:length(R011)-1 
+    diff = (R011(i+1)-R011(i))*T;
+    HR1(R011(i):R011(i+1)) = 1/diff;
+end
+
+HR2 = zeros(1,length(t_02));
+for i = 1:length(R021)-1 
+    diff = (R021(i+1)-R021(i))*T;
+    HR2(R021(i):R021(i+1)) = 60/diff;
+end
+
+figure(5)
+plot(t_01,HR1)
+
+
+figure(6)
+plot(t_02,HR2)
 
 
 %% Save R-wave detections for all signals
